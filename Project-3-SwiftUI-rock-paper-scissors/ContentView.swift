b61810a8ad4475ca6f46f565abeaa8a7e71118fb
kevin cuadros <kevindavidcuadros@gmail.com>
2024-10-03 19:39:24 -0500

    create animation

--- Project-3-SwiftUI-rock-paper-scissors/ContentView.swift
+++ Project-3-SwiftUI-rock-paper-scissors/ContentView.swift
@@ -9,44 +9,81 @@ import SwiftUI
 
 struct ContentView: View {
     
-    private var options: [String] = [ "✊", "🖐️", "✌️"]
+    @State private var options: [String] = [ "✊", "🖐️", "✌️"]
+    @State private var robotSelected = "🤖"
     @State private var optionSelected = ""
+    @State private var animate = false
     
     var body: some View {
         NavigationStack {
-            VStack(spacing: 35){
-                Text("🤖")
-                    .font(.system(size: 80))
-                
-                Text("Rock, Paper, Scissors?")
-                    .font(.title2)
-                    .fontWeight(.bold)
-                    .foregroundStyle(.white)
-                
-                HStack(spacing: 30){
-                    ForEach(options, id: \.self) { option in
-                        Button(option){
-                            optionSelected = option
-                        }
+            ZStack {
+                VStack(spacing: 35){
+                    Text(robotSelected)
                         .font(.system(size: 80))
+                    
+                    Text("Rock, Paper, Scissors?")
+                        .font(.title2)
+                        .fontWeight(.bold)
+                        .foregroundStyle(.white)
+                    
+                    HStack(spacing: 30){
+                        ForEach(options, id: \.self) { option in
+                            Button(option){
+                                onClickOption(option)
+                            }
+                            .font(.system(size: 80))
+                        }
                     }
+                    
+                    Spacer()
+                        .frame(height: 70)
                 }
-                
-                Spacer()
-                    .frame(height: 70)
-                
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .background(.indigo)
+
+                Rectangle()
+                    .fill(.green)
+                    .zIndex(99)
+                    .frame(width: 300, height: 400)
+                    .clipShape(.buttonBorder)
+                    .offset(x: 0, y: animate ? -20 : 700)
+                    .shadow(radius: 12)
             }
-            .frame(maxWidth: .infinity, maxHeight: .infinity)
-            .background(.indigo)
-            
             .toolbar {
                 ToolbarItem(placement: .principal) {
                     Text("Your selected \(optionSelected)")
                         .foregroundStyle(.white)
                         .fontWeight(.bold)
-                        .font(.title2)
+                        .font(.system(size: 30))
                 }
             }
+        }
+    }
+    
+    func onClickOption(_ option: String) {
+        optionSelected = option
+        robotSelected = options.randomElement() ?? ""
+        
+        withAnimation {
+            animate = true
+        }
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 3.0) {
+            robotSelected = "🤖"
+            optionSelected = ""
+            withAnimation {
+                animate = false
+            }
+        }
+    }
+    
+    func validateGame() {
+        if optionSelected == "✊" && robotSelected == "🖐️" {
+            
+        }
+        
+        if optionSelected == "✊" && robotSelected == "✌️" {
+            
         }
     }
     
